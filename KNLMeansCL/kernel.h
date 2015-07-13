bca30322dab99a4bdf6e0f7286b91f707fb940ff
Khanattila <edo.brunetti@gmail.com>
2015-07-13 18:19:50 +0200

    Weighted RGB distance

--- KNLMeansCL/kernel.h
+++ KNLMeansCL/kernel.h
@@ -57,8 +57,10 @@ static const char* source_code =
 "	if (NLMK_RGB) {																								  \n" \
 "		const float4 u1 = read_imagef(U1, smp, (int2) (x, y));													  \n" \
 "		const float4 u1_pq = read_imagef(U1_pq, smp, (int2) (x, y) + q);										  \n" \
-"		const float4 tmp = (u1 - u1_pq) * (u1 - u1_pq);															  \n" \
-"		U4[gidx] = tmp.x + tmp.y + tmp.z;																		  \n" \
+"		const float r = native_divide(u1.x + u1_pq.x, 6.0f);													  \n" \
+"		const float4 weight	= (float4) (2.0f/3.0f + r, 4.0f/3.0f, 1.0f - r, 0.0f);								  \n" \
+"		const float4 tmp = weight * ((u1 - u1_pq) * (u1 - u1_pq));												  \n" \
+"		U4[gidx] = tmp.x + tmp.y + tmp.z + tmp.w;																  \n" \
 "	} else {																									  \n" \
 "		const float u1 = read_imagef(U1, smp, (int2) (x, y)).x;													  \n" \
 "		const float u1_pq = read_imagef(U1_pq, smp, (int2) (x, y) + q).x;										  \n" \
