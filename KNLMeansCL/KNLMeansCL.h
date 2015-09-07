ebf2338f9dbecc8ffff219394c33cf5bf6e06ebf
Khanattila <edo.brunetti@gmail.com>
2015-09-07 19:17:20 +0200

    Version 0.6.3

--- KNLMeansCL/KNLMeansCL.h
+++ KNLMeansCL/KNLMeansCL.h
@@ -16,7 +16,7 @@
 *	along with KNLMeansCL. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#define VERSION "0.6.2"
+#define VERSION "0.6.3"
 
 #ifdef _MSC_VER
     #pragma warning (disable : 4514 4710 4820)
