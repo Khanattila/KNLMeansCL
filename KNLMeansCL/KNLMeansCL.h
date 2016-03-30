04d4e526493f65bfaeba26ff2f35248245ea7a94
Khanattila <edo.brunetti@gmail.com>
2016-03-30 17:22:50 +0200

    License

--- KNLMeansCL/KNLMeansCL.h
+++ KNLMeansCL/KNLMeansCL.h
@@ -1,19 +1,19 @@
 /*
 *	This file is part of KNLMeansCL,
-*	Copyright(C) 2015  Edoardo Brunetti.
+*	Copyright(C) 2015-2016  Edoardo Brunetti.
 *
 *	KNLMeansCL is free software: you can redistribute it and/or modify
-*	it under the terms of the GNU Lesser General Public License as published by
-*	the Free Software Foundation, either version 3 of the License, or
-*	(at your option) any later version.
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
 *
 *	KNLMeansCL is distributed in the hope that it will be useful,
 *	but WITHOUT ANY WARRANTY; without even the implied warranty of
 *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-*	GNU Lesser General Public License for more details.
+*	GNU General Public License for more details.
 *
-*	You should have received a copy of the GNU Lesser General Public License
-*	along with KNLMeansCL. If not, see <http://www.gnu.org/licenses/>.
+*	You should have received a copy of the GNU General Public License
+*   along with KNLMeansCL. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #define VERSION "0.7.5"
