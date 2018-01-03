8f94424eb04557e0b822fadd9b8647031b2eabfe
Khanattila <edo.brunetti@gmail.com>
2018-01-03 18:50:35 +0100

    Move the default values to the header

--- KNLMeansCL/KNLMeansCL.h
+++ KNLMeansCL/KNLMeansCL.h
@@ -16,7 +16,22 @@
 *    along with KNLMeansCL. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#define VERSION "1.1.0"
+#define VERSION           "1.1.0"
+#define DFT_d             1
+#define DFT_a             2
+#define DFT_s             4
+#define DFT_h             1.2f
+#define DFT_channels      "AUTO"
+#define DFT_wmode         0
+#define DFT_wref          1.0f
+#define DFT_ocl_device    "AUTO"
+#define DFT_ocl_id        0
+#define DFT_ocl_x         0
+#define DFT_ocl_y         0
+#define DFT_ocl_r         0
+#define DFT_lsb           false
+#define DFT_info          false 
+
 #define CL_USE_DEPRECATED_OPENCL_2_0_APIS
 
 #include <cstdint>
@@ -43,6 +58,7 @@
 
 #include <VapourSynth.h>
 #include <VSHelper.h>
+
 
 #ifdef __AVISYNTH_6_H__
 struct NLMAvisynth : public GenericVideoFilter {
