7d7de303b419f2838be1b6543fcec8fa6ccfd11b
Khanattila <edo.brunetti@gmail.com>
2018-01-05 16:07:51 +0100

    Increase the year

--- KNLMeansCL/KNLMeansCL.h
+++ KNLMeansCL/KNLMeansCL.h
@@ -1,6 +1,6 @@
 /*
 *    This file is part of KNLMeansCL,
-*    Copyright(C) 2015-2017  Edoardo Brunetti.
+*    Copyright(C) 2015-2018  Edoardo Brunetti.
 *
 *    KNLMeansCL is free software: you can redistribute it and/or modify
 *    it under the terms of the GNU General Public License as published by
