e1c7a350747b3564b0901c3c3c2f99637d8ec43a
Khanattila <edo.brunetti@gmail.com>
2018-01-21 20:22:02 +0100

    Default filter values

---
+++ KNLMeansCL/NLMDefault.h
@@ -0,0 +1,40 @@
+/*
+*    This file is part of KNLMeansCL,
+*    Copyright(C) 2015-2018  Edoardo Brunetti.
+*
+*    KNLMeansCL is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    KNLMeansCL is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with KNLMeansCL. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef NLM_DEFAULT_H
+#define NLM_DEFAULT_H
+
+//////////////////////////////////////////
+// Value Definition
+#define DFT_d             1
+#define DFT_a             2
+#define DFT_s             4
+#define DFT_h             1.2f
+#define DFT_channels      "AUTO"
+#define DFT_wmode         0
+#define DFT_wref          1.0f
+#define DFT_ocl_device    "AUTO"
+#define DFT_ocl_id        0
+#define DFT_ocl_x         0
+#define DFT_ocl_y         0
+#define DFT_ocl_r         0
+#define DFT_lsb           false
+#define DFT_info          false 
+#define VERSION           "1.1.0"
+
+#endif //__NLM_DEFAULT_H__
