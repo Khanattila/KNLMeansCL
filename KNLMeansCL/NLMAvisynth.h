8ef4a2f9a21f5b39f9ca7301e801283cd60780cd
Khanattila <edo.brunetti@gmail.com>
2018-01-21 20:20:47 +0100

    Only Avisynth stuff

--- KNLMeansCL/NLMAvisynth.h
+++ KNLMeansCL/NLMAvisynth.h
@@ -16,36 +16,14 @@
 *    along with KNLMeansCL. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef KNLMEANSCL_H
-#define KNLMEANSCL_H
-
-#define VERSION           "1.1.0"
-
-#define DFT_d             1
-#define DFT_a             2
-#define DFT_s             4
-#define DFT_h             1.2f
-#define DFT_channels      "AUTO"
-#define DFT_wmode         0
-#define DFT_wref          1.0f
-#define DFT_ocl_device    "AUTO"
-#define DFT_ocl_id        0
-#define DFT_ocl_x         0
-#define DFT_ocl_y         0
-#define DFT_ocl_r         0
-#define DFT_lsb           false
-#define DFT_info          false 
-
-#define CL_USE_DEPRECATED_OPENCL_2_0_APIS
+#ifndef NLM_AVISYNTH_H
+#define NLM_AVISYNTH_H
 
 #include "NLMKernel.h"
 
 #ifdef _WIN32
 #    include <avisynth.h>
 #endif
-
-#include <VapourSynth.h>
-#include <VSHelper.h>
 
 #ifdef __AVISYNTH_6_H__
 struct NLMAvisynth : public GenericVideoFilter {
@@ -76,27 +54,4 @@ public:
 };
 #endif //__AVISYNTH_6_H__
 
-#ifdef VAPOURSYNTH_H
-typedef struct NLMVapoursynth {
-public:
-    VSNodeRef *node, *knot;
-    const VSVideoInfo *vi;
-    int64_t d, a, s, wmode, ocl_id, ocl_x, ocl_y, ocl_r, info;
-    double wref, h;
-    const char *channels, *ocl_device;
-    bool pre_processing;
-    cl_uint clip_t, channel_num, idmn[2];
-    cl_platform_id platformID;
-    cl_device_id deviceID;
-    cl_context context;
-    cl_command_queue command_queue;
-    cl_program program;
-    cl_kernel kernel[NLM_KERNEL];
-    cl_mem mem_U[NLM_MEMORY], mem_P[3];
-    size_t hrz_result, vrt_result, dst_block[2], hrz_block[2], vrt_block[2];
-    bool equals(const VSVideoInfo *v, const VSVideoInfo *w);
-    void oclErrorCheck(const char* function, cl_int errcode, VSMap *out, const VSAPI *vsapi);
-} NLMVapoursynth;
-#endif //__VAPOURSYNTH_H__
-
-#endif //__KNLMEANSCL_H__
+#endif //__NLM_AVISYNTH_H__
